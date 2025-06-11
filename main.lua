517af009593211a83e1cfae30e84368ef057279a
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-06-12 04:34:36 +0530

    implemented toggling logic. needs a better rendering logic

--- main.lua
+++ main.lua
@@ -22,7 +22,7 @@ end
 
 -- Tuning Related
 local tunings = {
-	standard = { "E", "A", "D", "G", "B", "E" },
+	standard = { "E", "B", "G", "D", "A", "E" },
 }
 
 -- Fretboard Related
@@ -32,25 +32,60 @@ function Fretboard:new(tuning, frets)
 	local obj = {
 		tuning = tuning or tunings.standard,
 		frets = frets or 12,
+		notes = {},
 	}
+
+	-- Generate State for every Note
+	-- NOTE: here obj.property is used because self is not set yet!
+	for string_index, start_note in ipairs(obj.tuning) do
+		local start_index = Note.index_of(start_note)
+		obj.notes[string_index] = {}
+		for fret = 0, obj.frets do
+			local note_name = Note.name_at(start_index + fret, false)
+			obj.notes[string_index][fret] = {
+				name = note_name,
+				enabled = false,
+			}
+		end
+	end
+
+	-- Setup OOP
 	self.__index = self
 	setmetatable(obj, self)
 	return obj
 end
 
-function Fretboard:render()
-	for _, start_note in ipairs(self.tuning) do
-		local start_index = Note.index_of(start_note)
+function Fretboard:toggle(string_index, fret)
+	if string_index < 1 or string_index > 6 then
+		print(string.format("warning: must be 1 <= string index <= 6 (%s given)", string_index))
+		return
+	end
 
-		io.write(string.format("%-2s||  ", start_note))
-		for index = 1, self.frets do
-			local note = Note.name_at(start_index + index, false)
-			io.write(string.format("%-4s|", note))
+	if fret < 0 or fret > self.frets then
+		print(string.format("warning: must be 0 <= fret <= %d (%d given)", self.frets, fret))
+		return
+	end
+
+	self.notes[string_index][fret].enabled = not self.notes[string_index][fret].enabled
+end
+
+function Fretboard:render()
+	for string_index, str in ipairs(self.notes) do
+		local open_note = self.tuning[string_index]
+
+		io.write(open_note .. "|| ")
+
+		for fret = 1, self.frets do
+			local note = str[fret]
+			local display = note.enabled and "[" .. note.name .. "]" or note.name
+			display = #note.name == 2 and display or display .. " "
+			io.write(string.format("%-5s", display .. "|"))
 		end
 
 		io.write("\n")
 	end
 end
 
-local fb = Fretboard:new(tunings.standard, 12)
+local fb = Fretboard:new(tunings.standard, 17)
+fb:toggle(1, 0)
 fb:render()
