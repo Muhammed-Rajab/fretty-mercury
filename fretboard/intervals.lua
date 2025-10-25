2d429d0254926d8c81355aa78fac20d53c9ed0ee
Muhammed Rajab <muhammedrajab101@gmail.com>
2025-10-25 23:37:03 +0530

    removed junk from intervals

--- fretboard/intervals.lua
+++ fretboard/intervals.lua
@@ -13,21 +13,6 @@ local intervals = {
 	"M6",
 	"m7",
 	"M7",
-
-	-- Extensions
-	"9",
-	"b9",
-	"#9",
-	"11",
-	"#11",
-	"13",
-
-	-- Chord modifiers
-	"add9",
-	"sus2",
-	"sus4",
-	"no3",
-	"no5",
 }
 
 return intervals
